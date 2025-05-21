2b742e9d2d84c53417b284d47cabc231befc33ea
AnandBuyExchange <anand@buyexchange.in>
2025-05-21 15:29:36 +0530

    staff-order-page updated

--- app/(protected)/staff/dashboard/placeorder/page.tsx
+++ app/(protected)/staff/dashboard/placeorder/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import OrderDetailsForm from './order-details-form'
+import { Topbar } from '../../(components)/Topbar'
+import { pagesData } from '@/data/navigation'
+
 
 function page() {
   return (
-    <div className='container p-16'>
+    <div >
+      <Topbar pageData={pagesData.orderDetails}/>
         <OrderDetailsForm/>
+        
     </div>
   )
 }
