167229c1bc71b8b3028fd792dd6bea40079f349e
AnandBuyExchange <anand@buyexchange.in>
2025-05-21 13:46:07 +0530

    resolve build error

---
+++ app/(protected)/staff/dashboard/placeorder/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import OrderDetailsForm from './order-details-form'
+
+function page() {
+  return (
+    <div className='container p-16'>
+        <OrderDetailsForm/>
+    </div>
+  )
+}
+
+export default page
