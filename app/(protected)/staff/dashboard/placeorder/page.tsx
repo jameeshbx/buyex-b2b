486d4c7b07ff826226e2772d5ce2143e655beded
Jameesh Moidunny <jameesh.moidunny@endeavourtechnology.com>
2025-06-16 12:37:28 +0400

    Save quote in db

--- app/(protected)/staff/dashboard/placeorder/page.tsx
+++ app/(protected)/staff/dashboard/placeorder/page.tsx
@@ -1,21 +1,24 @@
-"use client"
+"use client";
 
-import React from 'react'
-import OrderDetailsForm from './order-details-form'
-import { Topbar } from '../../(components)/Topbar'
-import { pagesData } from '@/data/navigation'
-import BreadcrumbMenubar from '../../(components)/Menubar'
+import React from "react";
+import OrderDetailsForm from "./order-details-form";
+import { Topbar } from "../../(components)/Topbar";
+import { pagesData } from "@/data/navigation";
+import BreadcrumbMenubar from "../../(components)/Menubar";
+import { SessionProvider } from "next-auth/react";
 
 function page() {
   return (
     <div>
-      <Topbar pageData={pagesData.orderDetails}/>  
-      <BreadcrumbMenubar/>
-      <div className='p-4 bg-white'>
-        <OrderDetailsForm/>
+      <Topbar pageData={pagesData.orderDetails} />
+      <BreadcrumbMenubar />
+      <div className="p-4 bg-white">
+        <SessionProvider>
+          <OrderDetailsForm />
+        </SessionProvider>
       </div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
---
+++ prisma/migrations/20250616081925_pdffileadded/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "downloaded_quotes" ADD COLUMN     "generatedPDF" TEXT;
